475023e65f85bb4d21535f4cc397239baa368537
Gurjot Singh <gurjotsingh2004@gmail.com>
2025-07-03 15:52:51 +0530

    Changed footer

--- src/Applets/whatsapp.jsx
+++ src/Applets/whatsapp.jsx
@@ -6,20 +6,20 @@ import 'react-phone-number-input/style.css';
 
 export default function Whatsapp() {
     const [phone, setPhone] = useState("");
-    function handleClick(){
+    function handleClick() {
         const link = "https://wa.me/" + (phone.startsWith('+') ? phone.slice('+'.length) : phone)
         window.open(link, '_blank')
-        
+
     }
-    
+
 
     return (<>
         <Header />
-            <p className="m-12 mx-auto max-w-[30rem]">
+        <p className="m-12 mx-auto max-w-[30rem]">
             Send messages to any phone number on WhatsApp without the need to save the contact in your address book.
-            </p>
+        </p>
         <div className="flex flex-col gap-y-6 p-6 mt-6 max-w-[24rem] mx-auto">
-            
+
             <PhoneInput
                 defaultCountry="IN"
                 placeholder="Enter phone number"
@@ -27,7 +27,7 @@ export default function Whatsapp() {
                 onChange={setPhone}
                 className="flex border border-gray-300  px-3 py-2"
             />
-            
+
             <button className="bg-blue-600 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-lg shadow transition duration-200" onClick={handleClick}>
                 Chat
             </button>
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,12 +2,12 @@ export function Footer() {
     return (
       <footer className="bg-gray-100 text-gray-700 py-6 mt-[80rem]">
         <div className="max-w-7xl mx-auto px-4 text-center space-y-2">
-          <h2 className="text-xl font-semibold">MyAppName</h2>
+          <h2 className="text-xl font-semibold">Epyc Tools</h2>
           <p className="text-sm text-gray-600">
             A simple task management tool to streamline your workflow.
           </p>
           <p className="text-xs text-gray-500">
-            © {new Date().getFullYear()} MyAppName. All rights reserved.
+            © {new Date().getFullYear()} Epyc Tools. Made by Yolo_Twice.
           </p>
         </div>
       </footer>
